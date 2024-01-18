1c048b298bee8564b29c6297568ecf29849e2e9d
Usama <Usama248@gmail.com>
2024-01-18 16:03:51 +0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/login' },
+    {
+        path: 'login',
+        title: 'Log in to Gaudian MD',
+        loadComponent: () => import('./pages/auth/auth-layout/auth-layout.component').then(x => x.AuthLayoutComponent),
+        children: [
+          { path: '', title: 'Log in to Gaudian MD', loadComponent: () => import('./pages/auth/login/login.component').then(x => x.LoginComponent) },
+          { path: 'forgot-password', title: 'Reset your password', loadComponent: () => import('./pages/auth/forgot-password/forgot-password.component').then(x => x.ForgotPasswordComponent) },
+        ],
+    },
+];
